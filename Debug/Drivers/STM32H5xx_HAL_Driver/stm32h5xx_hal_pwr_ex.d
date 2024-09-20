5b0ec4d3804627b75b95e55633f3efae13cc97b2
Michal Tomek <tomekmichalhu@seznam.cz>
2024-09-20 17:38:41 +0200

    initial commit

---
+++ Debug/Drivers/STM32H5xx_HAL_Driver/stm32h5xx_hal_pwr_ex.d
@@ -0,0 +1,56 @@
+Drivers/STM32H5xx_HAL_Driver/stm32h5xx_hal_pwr_ex.o: \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Src/stm32h5xx_hal_pwr_ex.c \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal.h \
+ ../Core/Inc/stm32h5xx_hal_conf.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_rcc.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_def.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Device/ST/STM32H5xx/Include/stm32h5xx.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Device/ST/STM32H5xx/Include/stm32h503xx.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/core_cm33.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/mpu_armv8.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Device/ST/STM32H5xx/Include/system_stm32h5xx.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_rcc_ex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_gpio.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_gpio_ex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_icache.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_dma.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_dma_ex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_cortex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_flash.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_flash_ex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_pwr.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_pwr_ex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_tim.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_tim_ex.h \
+ /home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_exti.h
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal.h:
+../Core/Inc/stm32h5xx_hal_conf.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_rcc.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_def.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Device/ST/STM32H5xx/Include/stm32h5xx.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Device/ST/STM32H5xx/Include/stm32h503xx.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/core_cm33.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/cmsis_version.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Include/mpu_armv8.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/CMSIS/Device/ST/STM32H5xx/Include/system_stm32h5xx.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_rcc_ex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_gpio.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_gpio_ex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_icache.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_dma.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_dma_ex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_cortex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_flash.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_flash_ex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_pwr.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_pwr_ex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_tim.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_tim_ex.h:
+/home/thugmek/STM32Cube/Repository/STM32Cube_FW_H5_V1.3.0/Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_exti.h:
